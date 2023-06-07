c9e38cf0cb8ddfbb87625fbf8d3aec4a05224d8e
Kyle5 <fotusx@gmail.com>
2023-06-07 15:42:39 -0400

    Week 4

--- app/src/main/java/algonquin/cst2335/sinc0141/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/sinc0141/MainActivity.java
@@ -2,10 +2,14 @@ package algonquin.cst2335.sinc0141;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Switch;
 
@@ -14,11 +18,55 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     Switch switch1;
 
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w("MainActivity", "In onStart() - The Application is now visible on screen");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity", "In onResume() - The application is now responding to user input");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w("MainActivity", "In onPause() - The Application is no longer visible");
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w("MainActivity", "In onDestroy() - Any memory used by the application is freed");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w("MainActivity", "In onStop() - The Application is no longer visible");
+    }
+
+
+    private static String TAG = "MainActivity";
+
+    private Button loginButton ;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        Log.w("MainActivity", "In onCreate() - Loading Widgets");
+        Log.d( TAG, "Message");
+
+        loginButton = findViewById(R.id.loginButton);
+        imageView = findViewById(R.id.imageView);
+
+        loginButton.setOnClickListener( clk-> {
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(intent);
+                });
+
 
         imageView = findViewById(R.id.imageView);
         switch1 = findViewById(R.id.switch1);

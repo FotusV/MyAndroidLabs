fe9cd9be7ff3a78e0f51b5840264863114a9c22b
Kyle5 <fotusx@gmail.com>
2023-06-14 18:04:33 -0400

    Week 5 Fixing issues

--- app/src/main/java/algonquin/cst2335/sinc0141/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/sinc0141/MainActivity.java
@@ -1,98 +1,70 @@
 package algonquin.cst2335.sinc0141;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.FileOutputStream;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.activity_main);
 
-    ImageView imageView;
-    Switch switch1;
+  TextView textView = findViewById(R.id.textView);
+  EditText myEditText = findViewById(R.id.myEditText);
+  Button loginBtn = findViewById(R.id.loginBtn);
 
+  loginBtn.setOnClickListener( clk ->{
+   String password = myEditText.getText().toString();
 
+   checkPasswordComplexity( password );
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.w("MainActivity", "In onStart() - The Application is now visible on screen");
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.w("MainActivity", "In onResume() - The application is now responding to user input");
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.w("MainActivity", "In onPause() - The Application is no longer visible");
+  });
+ }
 
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.w("MainActivity", "In onDestroy() - Any memory used by the application is freed");
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.w("MainActivity", "In onStop() - The Application is no longer visible");
-    }
+ /**
+  *
+  * @param password The String object that we are checking
+  */
+  boolean checkPasswordComplexity(String password) {
 
+   boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
 
-    private static String TAG = "MainActivity";
+   foundUpperCase = foundLowerCase = foundNumber = foundSpecial = false;
 
-    private Button loginButton ;
-    private EditText emailEditText;
-    @SuppressLint({"WrongViewCast", "SetTextI18n"})
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.w("MainActivity", "In onCreate() - Loading Widgets");
-        Log.d( TAG, "Message");
+   if(!foundUpperCase)
+   {
 
-        loginButton = findViewById(R.id.loginButton);
-        imageView = findViewById(R.id.imageView);
-        emailEditText = findViewById(R.id.emailEditText);
-        loginButton.setOnClickListener( clk-> {
-                    Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
-                    nextPage.putExtra("EmailAddress", emailEditText.getText().toString());
-                    startActivity(nextPage);
-                });
+    Toast.makeText( "Missing Upper case letter" ) ;// Say that they are missing an upper case letter;
+
+    return false;
+
+   }
+
+   else if( ! foundLowerCase)
+   {
+    Toast.makeText( "Missing lower case letter" ); // Say that they are missing a lower case letter;
+
+    return false;
+
+   }
+
+   else if( ! foundNumber) { "Missing number"
+   Toast.makeText()
+   }
 
 
-        imageView = findViewById(R.id.imageView);
-        switch1 = findViewById(R.id.switch1);
+   else if(! foundSpecial) { "Missing special character" }
 
-        switch1.setOnCheckedChangeListener( (btn, isChecked) -> {
-            if (isChecked)
-            {
-                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(5000);
-                rotate.setRepeatCount(Animation.INFINITE);
-                rotate.setInterpolator(new LinearInterpolator());
+   else
 
-                imageView.startAnimation(rotate);
-            } else {
-                imageView.clearAnimation();
-            }
-        });
+    return true; //only get here if they're all true
 
-
-        };
-    }
+   return foundUpperCase;
+  }
+}

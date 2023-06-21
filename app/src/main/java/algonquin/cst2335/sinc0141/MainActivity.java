70004a2e7f9acdfe18224ed09bd23ee4548e36b1
Kyle5 <fotusx@gmail.com>
2023-06-21 16:54:12 -0400

    Added JavaDoc Comments and finished 5 test cases.

--- app/src/main/java/algonquin/cst2335/sinc0141/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/sinc0141/MainActivity.java
@@ -1,6 +1,5 @@
 package algonquin.cst2335.sinc0141;
 
-
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,6 +8,10 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * @Author: Kyle Sincennes Winch
+ * @Version 1.0
+ */
 public class MainActivity extends AppCompatActivity {
  @Override
  protected void onCreate(Bundle savedInstanceState) {
@@ -19,52 +22,58 @@ public class MainActivity extends AppCompatActivity {
   EditText myEditText = findViewById(R.id.myEditText);
   Button loginBtn = findViewById(R.id.loginBtn);
 
-  loginBtn.setOnClickListener( clk ->{
+  loginBtn.setOnClickListener(clk -> {
    String password = myEditText.getText().toString();
 
-   checkPasswordComplexity( password );
-
+   if (checkPasswordComplexity(password)) {
+    textView.setText("Your password meets the requirements");
+   } else {
+    textView.setText("You shall not pass!");
+   }
   });
  }
 
  /**
+  * Checks the complexity of the password.
   *
   * @param password The String object that we are checking
+  * @return true if the password is complex, false otherwise
   */
-  boolean checkPasswordComplexity(String password) {
+ boolean checkPasswordComplexity(String password) {
+  boolean foundUpperCase = false;
+  boolean foundLowerCase = false;
+  boolean foundNumber = false;
+  boolean foundSpecial = false;
 
-   boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
-
-   foundUpperCase = foundLowerCase = foundNumber = foundSpecial = false;
-
-   if(!foundUpperCase)
-   {
-
-    Toast.makeText( "Missing Upper case letter" ) ;// Say that they are missing an upper case letter;
-
-    return false;
-
+  for (char c : password.toCharArray()) {
+   if (Character.isUpperCase(c)) {
+    foundUpperCase = true;
+   } else if (Character.isLowerCase(c)) {
+    foundLowerCase = true;
+   } else if (Character.isDigit(c)) {
+    foundNumber = true;
+   } else {
+    foundSpecial = true;
    }
-
-   else if( ! foundLowerCase)
-   {
-    Toast.makeText( "Missing lower case letter" ); // Say that they are missing a lower case letter;
-
-    return false;
-
-   }
-
-   else if( ! foundNumber) { "Missing number"
-   Toast.makeText()
-   }
-
-
-   else if(! foundSpecial) { "Missing special character" }
-
-   else
-
-    return true; //only get here if they're all true
-
-   return foundUpperCase;
   }
+
+  /**
+   * Returns the following toasts if password doesn't meet requirements
+   */
+  if (!foundUpperCase) {
+   Toast.makeText(MainActivity.this, "Missing uppercase letter", Toast.LENGTH_SHORT).show();
+   return false;
+  } else if (!foundLowerCase) {
+   Toast.makeText(MainActivity.this, "Missing lowercase letter", Toast.LENGTH_SHORT).show();
+   return false;
+  } else if (!foundNumber) {
+   Toast.makeText(MainActivity.this, "Missing number", Toast.LENGTH_SHORT).show();
+   return false;
+  } else if (!foundSpecial) {
+   Toast.makeText(MainActivity.this, "Missing special character", Toast.LENGTH_SHORT).show();
+   return false;
+  } else {
+   return true; // Password is complex
+  }
+ }
 }

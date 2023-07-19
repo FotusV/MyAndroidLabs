92bdcbf8baa8ae3d1a1fd803053d4d082ec175d1
Kyle5 <fotusx@gmail.com>
2023-07-19 17:57:48 -0400

    Week 5 Commit Fixed tests not passing

--- app/src/androidTest/java/algonquin/cst2335/sinc0141/MainActivityTest.java
+++ app/src/androidTest/java/algonquin/cst2335/sinc0141/MainActivityTest.java
@@ -1,40 +1,30 @@
 package algonquin.cst2335.sinc0141;
 
+// textView myEditText loginBtn
 
-import androidx.test.espresso.DataInteraction;
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.ViewInteraction;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.filters.LargeTest;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
-import static androidx.test.InstrumentationRegistry.getInstrumentation;
-import static androidx.test.espresso.Espresso.onData;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.pressBack;
-import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static androidx.test.espresso.action.ViewActions.*;
-import static androidx.test.espresso.assertion.ViewAssertions.*;
-import static androidx.test.espresso.matcher.ViewMatchers.*;
-
-import algonquin.cst2335.sinc0141.R;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -46,121 +36,89 @@ public class MainActivityTest {
 
     @Test
     public void mainActivityTest() {
-        // Added a sleep statement to match the app's execution delay.
-        // The recommended way to handle such scenarios is to use Espresso idling resources:
-        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        // Delay execution for 500 milliseconds
         try {
             Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
+        // Set the password to "12345"
+        onView(withId(R.id.myEditText))
+                .perform(click())  // Click on the password field
+                .perform(replaceText("12345"))  // Enter the password "12345"
+                .perform(pressImeActionButton());  // Press the IME action button (e.g., "Done")
 
+        // Click the "Login" button
+        onView(withId(R.id.loginBtn))
+                .perform(click());
 
-        ViewInteraction appCompatEditText = onView( withId(R.id.myEditText));
-        appCompatEditText.perform(replaceText("12345"), closeSoftKeyboard());
+        // Verify that the correct error message is displayed
+        onView(withId(R.id.textView))
+                .check(matches(withText("You shall not pass!")));
+    }
 
-        ViewInteraction materialButton = onView( withId(R.id.loginBtn));
+    /**
+     * Test case to check if the password has a missing uppercase letter.
+     */
+    @Test
+    public void testFindMissingUppercase() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.myEditText));
+        appCompatEditText.perform(replaceText("password123#$*"));  // Set the password with missing uppercase
+
+        ViewInteraction materialButton = onView(withId(R.id.loginBtn));
         materialButton.perform(click());
 
-        ViewInteraction textView = onView( withId(R.id.textView));
-        textView.check(matches(withText("You shall not pass!")));
-    }
-/*
-Testing password inputs for when a password is wrong.
- */
-    @Test
-    public void testFindMissingUpperCase() {
-
-        // Step 1: Find the EditText and perform replacing the text and closing the keyboard
-        ViewInteraction editText = Espresso.onView(ViewMatchers.withId(R.id.myEditText));
-        editText.perform(replaceText("12345"), closeSoftKeyboard());
-
-        // Step 2: Find the button and perform clicking
-        ViewInteraction button = Espresso.onView(ViewMatchers.withId(R.id.loginBtn));
-        button.perform(click());
-
-        // Step 3: Find the TextView and check the text
-        ViewInteraction textView = Espresso.onView(ViewMatchers.withId(R.id.textView));
+        ViewInteraction textView = onView((withId(R.id.textView)));
         textView.check(matches(withText("You shall not pass!")));
     }
 
-    @Test
-    public void testPasswordMissingDigit() {
-        // Step 1: Find the EditText and perform replacing the text and closing the keyboard
-        ViewInteraction editText = Espresso.onView(ViewMatchers.withId(R.id.myEditText));
-        editText.perform(replaceText("Password#"), closeSoftKeyboard());
-
-        // Step 2: Find the button and perform clicking
-        ViewInteraction button = Espresso.onView(ViewMatchers.withId(R.id.loginBtn));
-        button.perform(click());
-
-        // Step 3: Find the TextView and check the text
-        ViewInteraction textView = Espresso.onView(ViewMatchers.withId(R.id.textView));
-        textView.check(matches(withText("Password should contain a digit.")));
-    }
-
-    @Test
-    public void testPasswordMissingUpperCase() {
-        // Step 1: Find the EditText and perform replacing the text and closing the keyboard
-        ViewInteraction editText = Espresso.onView(ViewMatchers.withId(R.id.myEditText));
-        editText.perform(replaceText("password1#"), closeSoftKeyboard());
-
-        // Step 2: Find the button and perform clicking
-        ViewInteraction button = Espresso.onView(ViewMatchers.withId(R.id.loginBtn));
-        button.perform(click());
-
-        // Step 3: Find the TextView and check the text
-        ViewInteraction textView = Espresso.onView(ViewMatchers.withId(R.id.textView));
-        textView.check(matches(withText("Password should contain an uppercase letter.")));
-    }
-
-    @Test
-    public void testPasswordMissingLowerCase() {
-        // Step 1: Find the EditText and perform replacing the text and closing the keyboard
-        ViewInteraction editText = Espresso.onView(ViewMatchers.withId(R.id.myEditText));
-        editText.perform(replaceText("PASSWORD1#"), closeSoftKeyboard());
-
-        // Step 2: Find the button and perform clicking
-        ViewInteraction button = Espresso.onView(ViewMatchers.withId(R.id.loginBtn));
-        button.perform(click());
-
-        // Step 3: Find the TextView and check the text
-        ViewInteraction textView = Espresso.onView(ViewMatchers.withId(R.id.textView));
-        textView.check(matches(withText("Password should contain a lowercase letter.")));
-    }
-
-    @Test
-    public void testPasswordMissingSpecialCharacter() {
-        // Step 1: Find the EditText and perform replacing the text and closing the keyboard
-        ViewInteraction editText = Espresso.onView(ViewMatchers.withId(R.id.myEditText));
-        editText.perform(replaceText("Password1"), closeSoftKeyboard());
-
-        // Step 2: Find the button and perform clicking
-        ViewInteraction button = Espresso.onView(ViewMatchers.withId(R.id.loginBtn));
-        button.perform(click());
-
-        // Step 3: Find the TextView and check the text
-        ViewInteraction textView = Espresso.onView(ViewMatchers.withId(R.id.textView));
-        textView.check(matches(withText("Password should contain a special character.")));
-    }
-
-    /*
-    this tests a working password
+    /**
+     * Test case to check if the password has a missing lowercase letter.
      */
-    public void testPasswordComplex() {
-        // Step 1: Find the EditText and perform replacing the text and closing the keyboard
-        ViewInteraction editText = Espresso.onView(ViewMatchers.withId(R.id.myEditText));
-        editText.perform(replaceText("Password123#$*"), closeSoftKeyboard());
+    @Test
+    public void testFindMissingLowercase() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.myEditText));
+        appCompatEditText.perform(replaceText("PASSWORD123#$*"));  // Set the password with missing lowercase
 
-        // Step 2: Find the button and perform clicking
-        ViewInteraction button = Espresso.onView(ViewMatchers.withId(R.id.loginBtn));
-        button.perform(click());
+        ViewInteraction materialButton = onView(withId(R.id.loginBtn));
+        materialButton.perform(click());
 
-        // Step 3: Find the TextView and check the text
-        ViewInteraction textView = Espresso.onView(ViewMatchers.withId(R.id.textView));
-        textView.check(matches(withText("Your password is complex enough.")));
+        ViewInteraction textView = onView((withId(R.id.textView)));
+        textView.check(matches(withText("You shall not pass!")));
     }
+
+    /**
+     * Test case to check if the password has a missing number.
+     */
+    @Test
+    public void testFindMissingNumber() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.myEditText));
+        appCompatEditText.perform(replaceText("Password#$*"));  // Set the password with missing number
+
+        ViewInteraction materialButton = onView(withId(R.id.loginBtn));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView((withId(R.id.textView)));
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    /**
+     * Test case to check if the password has a missing special character.
+     */
+    @Test
+    public void testFindMissingSpecialCharacter() {
+        ViewInteraction appCompatEditText = onView(withId(R.id.myEditText));
+        appCompatEditText.perform(replaceText("Password123"));  // Set the password with missing special character
+
+        ViewInteraction materialButton = onView(withId(R.id.loginBtn));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView((withId(R.id.textView)));
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+
 
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
@@ -179,5 +137,7 @@ Testing password inputs for when a password is wrong.
                         && view.equals(((ViewGroup) parent).getChildAt(position));
             }
         };
+
     }
+
 }

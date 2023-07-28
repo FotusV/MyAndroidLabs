cf78c666725584be64ebbabcc08cec542644bcbe
Kyle5 <fotusx@gmail.com>
2023-07-27 22:14:16 -0400

    Week 9 Commit Attempt of Fix

--- app/src/main/java/algonquin/cst2335/sinc0141/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/sinc0141/ChatRoom.java
@@ -8,9 +8,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,6 +17,8 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 import androidx.room.Room;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -32,7 +33,6 @@ import algonquin.cst2335.sinc0141.databinding.SentMessageBinding;
 import data.ChatMessageDAO;
 import data.ChatRoomViewModel;
 import data.MessageDatabase;
-import data.MessageDetailsFragment;
 
 
 /**
@@ -95,32 +95,11 @@ public class ChatRoom extends AppCompatActivity {
 
         setSupportActionBar(binding.myToolbar);
 
-
         MessageDatabase db = Room.databaseBuilder(getApplicationContext(), MessageDatabase.class, "database-name").build();
         mDAO = db.cmDAO();
 
         binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        chatModel.selectedMessage.observe(this, (newMessageValue) -> {
-            MessageDetailsFragment chatFragment = new MessageDetailsFragment(newMessageValue);
-
-            // Get the FragmentManager
-            FragmentManager fMgr = getSupportFragmentManager();
-
-            // Begin the FragmentTransaction
-            FragmentTransaction tx = fMgr.beginTransaction();
-
-            // Replace the existing fragment (if any) with the new one
-            tx.replace(R.id.fragmentLocation, chatFragment);
-
-            // Commit the transaction to apply the changes
-            tx.commit();
-
-
-        });
-
-
 
         chatModel = new ViewModelProvider(this).get(ChatRoomViewModel.class);
         ArrayList<ChatMessage> chatMessages = chatModel.messages.getValue();
@@ -252,26 +231,26 @@ public class ChatRoom extends AppCompatActivity {
 
 
 
-//                AlertDialog.Builder builder = new AlertDialog.Builder(itemView.getContext());
-//                builder.setMessage("Do you want to delete this message: " + chatMessage.getMessage());
-//                builder.setTitle("Question");
-//                builder.setNegativeButton("No", (dialog, cl) -> {
-//                });
-//                builder.setPositiveButton("Yes", (dialog, cl) -> {
-//
-//                    ChatMessage removedMessage = messages.get(position);
-//                    messages.remove(position);
-//                    myAdapter.notifyItemRemoved(position);
-//
-//                    Snackbar.make(itemView, "You deleted message #" + position, Snackbar.LENGTH_LONG)
-//                            .setAction("Undo", undoclk -> {
-//                                // Undo logic here
-//                                messages.add(position, removedMessage);
-//                                myAdapter.notifyItemInserted(position);
-//                            })
-//                            .show();
-//                });
-//                builder.create().show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(itemView.getContext());
+                builder.setMessage("Do you want to delete this message: " + chatMessage.getMessage());
+                builder.setTitle("Question");
+                builder.setNegativeButton("No", (dialog, cl) -> {
+                });
+                builder.setPositiveButton("Yes", (dialog, cl) -> {
+
+                    ChatMessage removedMessage = messages.get(position);
+                    messages.remove(position);
+                    myAdapter.notifyItemRemoved(position);
+
+                    Snackbar.make(itemView, "You deleted message #" + position, Snackbar.LENGTH_LONG)
+                            .setAction("Undo", undoclk -> {
+                                // Undo logic here
+                                messages.add(position, removedMessage);
+                                myAdapter.notifyItemInserted(position);
+                            })
+                            .show();
+                });
+                builder.create().show();
             });
         }
     }
--- app/src/main/java/data/ChatRoomViewModel.java
+++ app/src/main/java/data/ChatRoomViewModel.java
@@ -9,6 +9,4 @@ import algonquin.cst2335.sinc0141.ChatRoom;
 
 public class ChatRoomViewModel extends ViewModel {
     public MutableLiveData<ArrayList<ChatRoom.ChatMessage>> messages = new MutableLiveData<>();
-    public MutableLiveData<ChatRoom.ChatMessage> selectedMessage = new MutableLiveData< >();
-
 }
